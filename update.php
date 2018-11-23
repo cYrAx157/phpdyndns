8fe2efa884d2dc6e3f02d1359633a7f51999a85e
cYrAx157 <me@cyraxnet.de>
2018-11-23 10:31:14 +0100

    use popen instead of exec

--- update.php
+++ update.php
@@ -13,13 +13,13 @@ if (!isset($_SERVER['PHP_AUTH_USER'])) {
     exit;
 }
 
-$dns_server = 'localhost';
+$dns_server = '127.0.0.1';
 $ddns_fqdn = 'ddns.cyraxnet.de';
 $subdomain = $_SERVER['PHP_AUTH_USER'];
 $ttl = '86400';
 
-$zone = $subdomain . '.' . $domain;
-$cur_v4 = gethostbyname($zone);
+$zone = $subdomain . '.' . $ddns_fqdn;
+$curip_v4 = gethostbyname($zone);
 $curip_v6_rec = dns_get_record($zone, DNS_AAAA);
 $curip_v6 = $curip_v6_rec[0]['ipv6'];
 $newip_v4 = $_SERVER['REMOTE_ADDR'];
@@ -27,48 +27,33 @@ $newip_v6 = $_GET['v6'];
 $updv4_req = false;
 $updv6_req = false;
 
-$updatecmd_v4 = <<<EOF
-    server $dns_server
-    zone $zone
-    update delete $zone. A
-    update add $zone. $ttl A $newip_v6
-    send
-    exit
-EOF;
-
-$updatecmd_v6 = <<<EOF
-    server $dns_server
-    zone $zone
-    update delete $zone. AAAA
-    update add $zone. $ttl AAAA $newip_v6
-    send
-    exit
-EOF;
-
-if($curip_v4 != $newip_v4) {
-    print "Updateing A-Record (IPv4) on Zone $zone (Old-IP: $curip_v4 -> New-IP $newip_v4)";
-    $updv4_req = true;
-}
-
-if($curip_v6 != $newip_v6) { 
-    print "Updateing A-Record (IPv6) on Zone $zone (Old-IP: $curip_v6 -> New-IP $newip_v6)";
-    $updv6_req = true;    
-}
-
-if(!$updv4_req && !$updv6_req) {
+if($curip_v4 == $newip_v4 && $curip_v6 == $newip_v6) {
 
 	echo '200 Status OK ! No Update required';
 
   exit;
 }
 
-if($updv6_req == true) {
+$nsupdate = popen("/usr/bin/nsupdate -k /etc/named/ddns-key.ddns.cyraxnet.de.conf", "w");
+fwrite($nsupdate, "server ".$dns_server."\n");
+fwrite($nsupdate, "zone ".$zone."\n");  
 
-    exec("/usr/bin/nsupdate -k /etc/named/ddns-key.ddns.cyraxnet.de.conf $updatecmd_v6", $cmdout, $retval);
-    print "IPv6-Update - Return code = " . $retval . "\n";
-}
+    if($curip_v4 != $newip_v4) { 
+        print "Updating A-Record (IPv4) on Zone $zone (Old-IP: $curip_v4 -> New-IP $newip_v4)";
+        fwrite($nsupdate, "update delete ".$zone.".A\n");
+        fwrite($nsupdate, "update add ".$zone.". ".$ttl." A ".$newip_v4."\n");   
+    }
+    if($curip_v6 != $newip_v6) { 
+        print "Updating AAAA-Record (IPv6) on Zone $zone (Old-IP: $curip_v6 -> New-IP $newip_v6)";
+        fwrite($nsupdate, "update delete ".$zone.".AAAA\n");
+        fwrite($nsupdate, "update add ".$zone.". ".$ttl." AAAAA ".$newip_v6."\n");   
+    }
+    
+fwrite($nsupdate, "send\n");
+fwrite($nsupdate, "quit\n");
+echo 'Update done';
+pclose($nsupdate);
 
-exec("/usr/bin/nsupdate -k /etc/named/ddns-key.ddns.cyraxnet.de.conf $updatecmd_v4", $cmdout, $retval);
-print "IPv4-Update - Return code = " . $retval . "\n";
+// print "IPv4-Update - Return code = " . $retval . "\n";
 
 ?>
