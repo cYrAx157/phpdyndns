60b9415062282d8f90197bf513a02ee31f8b172c
cYrAx157 <me@cyraxnet.de>
2018-11-29 18:08:53 +0100

    Add logging to syslog

--- update.php
+++ update.php
@@ -9,7 +9,7 @@
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="cYrAx DDNS-Realm"');
     header('HTTP/1.0 401 Unauthorized');
-    echo '401 ERROR (No authorization informationen provided !';
+    echo '401 ERROR (No authorization informationen provided)';
     exit;
 }
 
@@ -18,6 +18,7 @@ $ddns_fqdn = 'ddns.cyraxnet.de';
 $subdomain = $_SERVER['PHP_AUTH_USER'];
 $update_key = '/etc/named/ddns-key.ddns.cyraxnet.de.conf';
 $ttl = '86400';
+$logging = true;
 
 $zone = $subdomain . '.' . $ddns_fqdn;
 $curip_v4 = gethostbyname($zone);
@@ -35,22 +36,50 @@ if($curip_v4 == $newip_v4 && $curip_v6 == $newip_v6) {
   exit;
 }
 
+if($logging == true) {
+
+    openlog("DNS-Updater", LOG_PID | LOG_PERROR, LOG_DAEMON);
+
+}
+
 $nsupdate = popen("/usr/bin/nsupdate -k $update_key", "w");
 fwrite($nsupdate, "server ".$dns_server."\n");
 fwrite($nsupdate, "zone ".$ddns_fqdn.".\n");  
 
     if($curip_v4 != $newip_v4) { 
         print "Updating A-Record (IPv4) on Zone $zone (Old-IP: $curip_v4 -> New-IP $newip_v4)<br>";
-        fwrite($nsupdate, "update delete ".$zone.". A\n");
-        fwrite($nsupdate, "update add ".$zone.". ".$ttl." A ".$newip_v4."\n");   
-    }
+        $fwrite = fwrite($nsupdate, "update delete ".$zone.". A\n");
+        $fwrite = fwrite($nsupdate, "update add ".$zone.". ".$ttl." A ".$newip_v4."\n");   
+
+        if($logging == true) {
+            if ($fwrite == false) {
+                print "ERROR ! Update Failed !";
+                syslog(LOG_ERR,"ERROR ! Update Failed !");
+            } else {
+                print "Update A-Record successful ! (Old-IP: $curip_v4 -> New-IP $newip_v4)";
+                syslog(LOG_INFO,"Update A-Record successful ! (Old-IP: $curip_v4 -> New-IP $newip_v4)");
+                }
+            }
+        }
+
   if (isset($newip_v6)) {
     if($curip_v6 != $newip_v6) { 
         print "Updating AAAA-Record (IPv6) on Zone $zone (Old-IP: $curip_v6 -> New-IP $newip_v6)<br>";
-        fwrite($nsupdate, "update delete ".$zone.". AAAA\n");
-        fwrite($nsupdate, "update add ".$zone.". ".$ttl." AAAA ".$newip_v6."\n");   
-  }
+        $fwrite = fwrite($nsupdate, "update delete ".$zone.". AAAA\n");
+        $fwrite = fwrite($nsupdate, "update add ".$zone.". ".$ttl." AAAA ".$newip_v6."\n");
+        
+        if($logging == true) {
+            if ($fwrite == false) {
+                print "ERROR ! Update Failed !";
+                syslog(LOG_ERR,"ERROR ! Update Failed !");
+            } else {
+                print "Update AAAA-Record successful ! (Old-IP: $curip_v6 -> New-IP $newip_v6)";
+                syslog(LOG_INFO,"Update AAAA-Record successful ! (Old-IP: $curip_v6 -> New-IP $newip_v6)");
+                closelog();
+                }
+        }
     }
+  }
 
 fwrite($nsupdate, "send\n");
 fwrite($nsupdate, "quit\n");
